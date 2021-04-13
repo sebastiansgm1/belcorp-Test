b47a468ee1b54d1a1bca4248b892651a3ee86906
sebastiangaleano <sebastian.galeano@rappi.com>
2021-04-13 11:59:40 -0500

    Se eliminan metodos no usados y se corrige el properties

--- src/main/java/com/co/belcorp/pages/GeneralPage.java
+++ src/main/java/com/co/belcorp/pages/GeneralPage.java
@@ -11,21 +11,8 @@ public class GeneralPage extends PageObject {
     getDriver().get(url);
   }
 
-  private static final String SCROLL_INSTRUCTION = "window.scrollBy(0, document.body.scrollHeight)";
-
-  public void scrollDown() {
-    JavascriptExecutor executor = (JavascriptExecutor) this.getDriver();
-    executor.executeScript(
-        SCROLL_INSTRUCTION, "");
-  }
-
-  public void scrollToElement(WebElement element) {
-    new Actions(getDriver()).moveToElement(element).perform();
-  }
-
   protected void moveToElement(WebElement element) {
     Actions actionProvider = new Actions(getDriver());
     actionProvider.moveToElement(element).build().perform();
-    ;
   }
 }

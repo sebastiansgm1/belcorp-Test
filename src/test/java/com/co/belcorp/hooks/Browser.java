143bb0fb27cbbb6f34b45978f759aa466a74fb85
SebasGaleano <sebastiangaleano@belcorp.biz>
2021-05-19 13:02:05 -0500

    Se implementa webDriver para lambdatest

---
+++ src/test/java/com/co/belcorp/hooks/Browser.java
@@ -0,0 +1,42 @@
+package com.co.belcorp.hooks;
+
+import net.thucydides.core.webdriver.DriverSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.HashMap;
+
+public class Browser implements DriverSource {
+
+    @Override
+    public WebDriver newDriver() {
+        DesiredCapabilities options = new DesiredCapabilities();
+        String downloadFilepath = System.getProperty("user.dir") + "\\src\\test\\resources\\download";
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        options.setCapability("browserName", "Chrome");
+        options.setCapability("platform", "Windows 8.1");
+        options.setCapability("version", "87.0");
+        options.setCapability("resolutionl", "1024x768");
+        options.setCapability("name", "First Test");
+        options.setCapability("build", "Sample Test");
+        options.setCapability("network", "true");
+        options.setCapability("visual", "true");
+        options.setCapability("video", "true");
+        options.setCapability("console", "true");
+        try {
+            return new RemoteWebDriver(new URL("https://elionavarrete:rwLlFCESHtobr8vm6iZ4qv4HgNln8bytli9P2L6Glx77cvqkDH@hub.lambdatest.com/wd/hub"), options);
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean takesScreenshots() {
+        return false;
+    }
+}

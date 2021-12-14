237816df09b172235d5730ad095f262d6b8a40a7
sebastiangaleano <84464550+sebastiangaleano@users.noreply.github.com>
2021-12-14 14:01:50 -0500

    Update Browse.java
    delete comments into the class

--- src/test/java/com/co/belcorp/hooks/Browser.java
+++ src/test/java/com/co/belcorp/hooks/Browser.java
@@ -92,32 +92,4 @@ public class Browser implements DriverSource {
     return false;
   }
 }
-//public class Browser implements DriverSource {
-//
-//    @Override
-//    public WebDriver newDriver() {
-//        DesiredCapabilities options = new DesiredCapabilities();
-//        HashMap<String, Object> chromePrefs = new HashMap<>();
-//        options.setCapability("browserName", "Chrome");
-//        options.setCapability("platform", "Windows 8.1");
-//        options.setCapability("version", "87.0");
-//        options.setCapability("resolutionl", "1024x768");
-//        options.setCapability("name", "First Test");
-//        options.setCapability("build", "Sample Test");
-//        options.setCapability("network", "true");
-//        options.setCapability("visual", "true");
-//        options.setCapability("video", "true");
-//        options.setCapability("console", "true");
-//        try {
-//            return new RemoteWebDriver(new URL("https://elionavarrete:rwLlFCESHtobr8vm6iZ4qv4HgNln8bytli9P2L6Glx77cvqkDH@hub.lambdatest.com/wd/hub"), options);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            return null;
-//        }
-//    }
-//
-//    @Override
-//    public boolean takesScreenshots() {
-//        return false;
-//    }
-//}
+
